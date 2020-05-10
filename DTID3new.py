2dfd794a3aee17d72e801053984180a2d1fa9d51
lamphucnghi313 <lamphucnghi@gmail.com>
2020-05-11 03:09:34 +0700

    Destroy the old version.

--- DTID3new.py
+++ DTID3new.py
@@ -1,140 +1,93 @@
-from Working_with_file import read_file, write_file
+from Working_with_file import read_file, write_file, make_folder
 import math
+from collections import defaultdict
 
-inputFolder = 'input'
-inputFileName = 'weather'
-outFileFolder = 'outfile'
-fileLogName = 'log'
+folderInput = 'input'
+fileInput = 'weather'
+folderOutFile = 'outfile'
+fileLog = 'log'
 fileType = ['.txt']
 splitType = [', ', ' ']
 
-def count_each_value_of_attribute(objectList: list(list()), attributePos: int) -> dict:
-    attrNameDict = dict()
-    for objectIndex in range(0, len(objectList)):
-        if objectList[objectIndex][attributePos] not in attrNameDict:
-            attrNameDict[objectList[objectIndex][attributePos]] = 1
-        else:
-            attrNameDict[objectList[objectIndex][attributePos]] += 1
-    #co nen dem thu xem cac attrName bang nhau ve so luong, giam tai viec tinh entropy (vi se = 1).
-    return attrNameDict
+class TreeNode():
+    def __init__(self, name:str, isLeaf: bool):
+        self.name = name
+        self.isLeaf = isLeaf
+        self.childs = None # dict of treenode (k:value of Attribute, v:TreeNode)
 
-
-'''
-De tinh entropy, input la bang cac doi tuong va vi tri thuoc tinh phan lop,
-tra ve gia tri float
-'''
-def entropy(objectList: list(list()), classifyPos: int) -> float:
-    if len(objectList) <= 1:
-        return -1.0
-    entropy = 0.0
-    classifyNameDict = count_each_value_of_attribute(objectList, classifyPos)
-    objectCount = len(objectList)
-    if len(classifyNameDict) <= 1:
-        return 0
-    for (classifyName, classifyCount) in classifyNameDict.items():
-        entropy -= (classifyCount/objectCount)*math.log2(classifyCount/objectCount)
-    return entropy
-
-
-'''
-Entropy of attribute A after deciding on a particular attribute B.
-- Lay danh sach cac gia tri cua thuoc tinh.
-- Rut trich cac bang con theo cac gia tri do.
-- Tinh entropy cho tung gia tri cua thuoc tinh.
-- Tinh Entropy toan bo gia tri cua thuoc tinh.
-'''
-def entropy_by_attribute(objectList: list(list()), attrPos: int, classifyPos: int) -> float:
-    entropyByAttr = 0.0
-    valueCountOfAttrDict = count_each_value_of_attribute(objectList, attrPos)
-    
-    objectListByEachValueOfAttr = extract_objectList_by_value_of_attribute(objectList, attrPos)
-    
-    entropyAtEachValue = dict()
-    for objectListAtValue in objectListByEachValueOfAttr:
-        entrAtVal = entropy(objectListAtValue, classifyPos)
-        if entrAtVal != -1:
-            # objectListAtValue[0][attrPos]
-            # 0: first data row
-            # objectListAtValue[0][attrPos]: value of attribute
-            entropyAtEachValue[objectListAtValue[0][attrPos]] = entrAtVal
-    
-    for (valueName, entrByVal) in entropyAtEachValue.items():
-        entropyByAttr += (valueCountOfAttrDict[valueName]/len(objectList))*entrByVal
+class DTreeID3():
+    def __init__(self, listInput: list, posClassAttr: int):
+        self.root = None
+        self.listData = listInput
+        self.listAttrName = self.listData[0]
+        self.listObject = self.listData[1:]
+        self.indexClassifyAttr = posClassAttr
         
-    return entropyByAttr, entropyAtEachValue#list(valueCountOfAttrDict.keys())
-
-
-'''
-Information Gain.
-'''
-def information_gain(entropyOverall: float, entropyAttr: float) -> float:
-    return (entropyOverall - entropyAttr)
-
-
-'''
-Rut trich tu bang ban dau, theo gia tri cua thuoc tinh.
-'''
-def extract_objectList_by_value_of_attribute(objectList: list(list()), attributePos: int):
-    attrNameDict = count_each_value_of_attribute(objectList, attributePos)
-    objectListByEachValueOfAttr = list(list(list()))
-    for (attrName, count) in attrNameDict.items():
-        valueOfAttrList = list()
-        for objIndex in range(0, len(objectList)):
-            if objectList[objIndex][attributePos] == attrName:
-                valueOfAttrList.append(objectList[objIndex])
-        objectListByEachValueOfAttr.append(valueOfAttrList)
-    
-    return objectListByEachValueOfAttr
-
-def get_max_of_dict(inpDict: dict):
-    maxV = -1
-    maxK = ''
-    for (k,v) in inpDict.items():
-        if v > maxV:
-            maxV = v
-            maxK = k
-    return (maxK, maxV)
-
-def run(inputList: list, attrVisitedList: list):
-    classifyPos = -1
-    # inputList = read_file.read_csv_to_list_of_row(inputFolder, inputFileName)
-    objectList = inputList[1:]
-    attrNameList = inputList[0]
-    entropyOverall = entropy(objectList, classifyPos)
-    
-    write_file.list_to_txt_continuos(['Entropy(S):', str(entropyOverall)], outFileFolder, fileLogName, fileType[0], splitType[1])
-    
-    # IGList = list()
-    IGDict = dict()
-    attrCount = len(objectList[0]) - 2 # Tru cot stt va cot phan lop.
-    for attrIndex in range(1, attrCount + 1):
-        if attrIndex not in attrVisitedList:
-            entropyByAttr = entropy_by_attribute(objectList, attrIndex, -1)
-            entropybyValueList = entropyByAttr[1]
-            entropyAttr = entropyByAttr[0]
-            iGAttr = information_gain(entropyOverall, entropyAttr)
-            # IGList.append(iGAttr)
-            IGDict[attrNameList[attrIndex]] = iGAttr
-            write_file.list_to_txt_continuos(['IG({0}):{1}. Entropy({0}):{2}'.format(attrNameList[attrIndex], iGAttr, entropyAttr), entropybyValueList], outFileFolder, fileLogName, fileType[0], splitType[1])
-
-    (attrMaxName, maxIG) = get_max_of_dict(IGDict)
-
-    write_file.list_to_txt_continuos(['Max IG({0}):'.format(attrMaxName), maxIG], outFileFolder, fileLogName, fileType[0], splitType[1])
-    
-    return (attrMaxName, maxIG)
+    def _calc_entropy(self, listObject: list) -> float:
+        entropy = 0.0
+        dictValueClassifyCount = self._count_object_each_value_of_attribute(listObject, self.indexClassifyAttr)
         
+        totalObject = len(listObject)
+        for (value, count) in dictValueClassifyCount.items():
+            prob = count/totalObject # K can rang buoc vi kiem tra listData tu luc bat dau
+            entropy -= prob*(math.log2(prob))
+
+        return entropy
+
+    def _count_object_each_value_of_attribute(self, listObject: list, indexAttr: int) -> dict:
+        dictValueCount = dict()
+        for row in listObject:
+            value = row[indexAttr]
+            if value not in dictValueCount:
+                dictValueCount[value] = 1
+            else:
+                dictValueCount[value] += 1
+        return dictValueCount
+
+    def _extract_object_by_value_of_attribute(self, listObject: list, posAttr: int) -> list:
+        listObjectEachValue = list()
+        listValue = list()
+        for row in listObject:
+            value = row[posAttr]
+            if value not in listValue:
+                listValue.append(value)
+                listObjectEachValue.append([row])
+            else:
+                indexValue = listValue.index(value)
+                listObjectEachValue[indexValue].append(row)
+        return (listObjectEachValue, listValue)
+
+
+
+    def _run(self):
+        entropyS = self._calc_entropy(self.listObject)
+        dictEntropyAttr = defaultdict(dict)
+        for posAttr in range(1, len(self.listAttrName)-1):
+            (listObjectEachValue, listValue) = self._extract_object_by_value_of_attribute(self.listObject, posAttr)
+            dictEntropyOfValue = dict()
+            for listObjectByValue in listObjectEachValue:
+                value = listValue[listObjectEachValue.index(listObjectByValue)]
+                dictEntropyOfValue[value] = self._calc_entropy(listObjectByValue)
+            attrName = self.listAttrName[posAttr]
+            dictEntropyAttr[attrName] = dictEntropyOfValue
+        print(dictEntropyAttr)
+
+
+        return 1
+    
+
+
+    
+
+def _read_file(inputFolder: str, inputFileName: str) -> list:
+    inputList = read_file.read_csv_to_list_of_row(inputFolder, inputFileName)
+    return inputList
 
 
 def main():
-    attrVisitedList = [0, -1]
-    attrPos = 1
-    inputList = read_file.read_csv_to_list_of_row(inputFolder, inputFileName)
-    print(run(inputList, attrVisitedList))
-    attrVisitedList.append(1)
-    attrPos += 1
-    inputList2 = extract_objectList_by_value_of_attribute(inputList, attrPos)
-    attrNameList = inputList2[0]
-    print(run(attrNameList + inputList2[1], attrVisitedList))
+    listData = _read_file(folderInput, fileInput)
+    indexClassifyAttribute = -1
+    id3 = DTreeID3(listData, indexClassifyAttribute)
+    id3._run()
 
-    
 if __name__ == "__main__": main()

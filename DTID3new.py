0423996a8de9fd53b2af8a8e3c780b4b86dd8f4f
lamphucnghi313 <lamphucnghi@gmail.com>
2020-05-14 04:06:26 +0700

    Destroy the lasted version. Make the new version complete.

--- DTID3new.py
+++ DTID3new.py
@@ -3,17 +3,26 @@ import math
 from collections import defaultdict
 
 folderInput = 'input'
-fileInput = 'weather'
+fileInput = ['weather', 'Bai2', 'BuyComputer', 'BHK']
 folderOutFile = 'outfile'
 fileLog = 'log'
 fileType = ['.txt']
 splitType = [', ', ' ']
 
 class TreeNode():
-    def __init__(self, name:str, isLeaf: bool):
+    def __init__(self, name: str, sizeListFlag: int):
         self.name = name
-        self.isLeaf = isLeaf
-        self.childs = None # dict of treenode (k:value of Attribute, v:TreeNode)
+        self.isLeaf = False
+        self.childs = dict() # dict of treenode (k:value of Attribute, v:TreeNode)
+        self.listObject = None
+        self.listAttrFlag = list()
+        for i in range(sizeListFlag):
+            self.listAttrFlag.append(0)
+        self.listAttrFlag[0] = -1
+        self.listAttrFlag[-1] = -1
+        self.entropy = 0.0
+        self.infoGain = 0.0
+        
 
 class DTreeID3():
     def __init__(self, listInput: list, posClassAttr: int):
@@ -22,20 +31,26 @@ class DTreeID3():
         self.listAttrName = self.listData[0]
         self.listObject = self.listData[1:]
         self.indexClassifyAttr = posClassAttr
+        self.totalAttr = len(self.listAttrName)
+        self.totalObject = len(self.listData) - 1
+        self.countObjectClassified = 0
         
     def _calc_entropy(self, listObject: list) -> float:
         entropy = 0.0
-        dictValueClassifyCount = self._count_object_each_value_of_attribute(listObject, self.indexClassifyAttr)
+        dictValueClassifyCount = self._count_object_each_value_of_attribute(listObject, self.listAttrName[self.indexClassifyAttr])
         
         totalObject = len(listObject)
         for (value, count) in dictValueClassifyCount.items():
+            if count == totalObject:
+                return 0.0
             prob = count/totalObject # K can rang buoc vi kiem tra listData tu luc bat dau
             entropy -= prob*(math.log2(prob))
 
         return entropy
 
-    def _count_object_each_value_of_attribute(self, listObject: list, indexAttr: int) -> dict:
+    def _count_object_each_value_of_attribute(self, listObject: list, nameAttr: str) -> dict:
         dictValueCount = dict()
+        indexAttr = self.listAttrName.index(nameAttr)
         for row in listObject:
             value = row[indexAttr]
             if value not in dictValueCount:
@@ -44,11 +59,12 @@ class DTreeID3():
                 dictValueCount[value] += 1
         return dictValueCount
 
-    def _extract_object_by_value_of_attribute(self, listObject: list, posAttr: int) -> list:
+    def _extract_object_by_value_of_attribute(self, listObject: list, nameAttr: str) -> list:
         listObjectEachValue = list()
         listValue = list()
+        indexAttr = self.listAttrName.index(nameAttr)
         for row in listObject:
-            value = row[posAttr]
+            value = row[indexAttr]
             if value not in listValue:
                 listValue.append(value)
                 listObjectEachValue.append([row])
@@ -57,26 +73,126 @@ class DTreeID3():
                 listObjectEachValue[indexValue].append(row)
         return (listObjectEachValue, listValue)
 
+    def _calc_entropy_attribute(self, listObject: list, nameAttr: str):
+        dictEntropyOfValue = dict()
+        entropyAttr = 0.0
+        (listObjectEachValue, listValue) = self._extract_object_by_value_of_attribute(listObject, nameAttr)
+        for listObjectByValue in listObjectEachValue:
+            indexVal = listObjectEachValue.index(listObjectByValue)
+            value = listValue[indexVal]
+            entropyVal = self._calc_entropy(listObjectByValue)
+            dictEntropyOfValue[value] = entropyVal
+            
+            probAppearance = len(listObjectByValue)/self.totalObject
+            entropyAttr += probAppearance*entropyVal
+        print('Entropy({0}) : {1}'.format(nameAttr, entropyAttr))
+        print(dictEntropyOfValue)
+        return (entropyAttr, dictEntropyOfValue)
 
+    def _set_root(self):
+        entropyS = self._calc_entropy(self.listObject)
+        print('Entropy(S): {0}'.format(entropyS))
+        dictInfoGainAttr = dict()
+        listAttrName = self.listAttrName[1:self.totalAttr-1]
+        for nameAttr in listAttrName:
+            (entropyAttr, dictEntropyOfValue) = self._calc_entropy_attribute(self.listObject, nameAttr)
+            infoGain = entropyS - entropyAttr
+            dictInfoGainAttr[nameAttr] = infoGain
+            print('Information Gain({0}): {1}\n'.format(nameAttr, infoGain))
+        (attr, ig) = _get_max_of_dict(dictInfoGainAttr)
+        print('MAX(ig): {0}: {1}    ============\n'.format(attr, ig))
+
+        self.root = TreeNode(attr, self.totalAttr)
+        self.root.isLeaf = False
+        self.root.entropy = entropyS
+        self.root.infoGain = ig
+        self.root.listObject = self.listObject
+        self.root.listAttrFlag[self.listAttrName.index(attr)] = 1
+
+    def _find_separate_node(self, listObject: list, prevNode: TreeNode):
+        namePrevAttr = prevNode.name
+        valueOfAttr = listObject[0][self.listAttrName.index(namePrevAttr)]
+
+        entropyS = self._calc_entropy(listObject)
+        print('Entropy({0}={1}): {2}'.format(namePrevAttr, valueOfAttr, entropyS))
+
+        if entropyS == 0:
+            classifyValueName = listObject[0][-1]
+            child = TreeNode(classifyValueName, self.totalAttr)
+            child.listAttrFlag = prevNode.listAttrFlag.copy()
+            child.listAttrFlag[self.listAttrName.index(namePrevAttr)] = 1
+            child.listObject = listObject
+            child.isLeaf = True
+            
+            prevNode.childs[valueOfAttr] = child
+            
+            self.countObjectClassified += len(listObject)
+            print('Leaf: {0}    ============\n'.format(child.name))
+            # return child
+        else:
+            dictInfoGainAttr = dict()
+            dictEntropyAttr = dict()
+            # TIm danh sach thuoc tinh can tinh entropy
+            listAttrFlag = prevNode.listAttrFlag
+            for nameAttr in self.listAttrName:
+                if listAttrFlag[self.listAttrName.index(nameAttr)] == 0:
+                    (entropyAttr, dictEntropyOfValue) = self._calc_entropy_attribute(listObject, nameAttr)
+                    infoGain = entropyS - entropyAttr
+                    dictEntropyAttr[nameAttr] = entropyAttr
+                    dictInfoGainAttr[nameAttr] = infoGain
+                    print('Information Gain({0}): {1}\n'.format(nameAttr, infoGain))
+            (attr, ig) = _get_max_of_dict(dictInfoGainAttr)
+            print('MAX(ig): {0}: {1}    ============\n'.format(attr, ig))
+
+            child = TreeNode(attr, self.totalAttr)
+            child.listAttrFlag = prevNode.listAttrFlag.copy()
+            child.listAttrFlag[self.listAttrName.index(namePrevAttr)] = 1
+            child.listObject = listObject
+            child.entropy = dictEntropyAttr[attr]
+            child.infoGain = ig
+            
+            prevNode.childs[valueOfAttr] = child
+        
+        return child
+        
+    def _find_list_separate_node(self, curNode: TreeNode):
+        nameCurAttr = curNode.name
+        listSepNode = list()
+        (listObjectEachValue, listValue) = self._extract_object_by_value_of_attribute(curNode.listObject, nameCurAttr)
+        for listObjectByValue in listObjectEachValue:
+            sepNode = self._find_separate_node(listObjectByValue, curNode)
+            if sepNode.isLeaf == False:
+                listSepNode.append(sepNode)
+        return listSepNode
 
     def _run(self):
-        entropyS = self._calc_entropy(self.listObject)
-        dictEntropyAttr = defaultdict(dict)
-        for posAttr in range(1, len(self.listAttrName)-1):
-            (listObjectEachValue, listValue) = self._extract_object_by_value_of_attribute(self.listObject, posAttr)
-            dictEntropyOfValue = dict()
-            for listObjectByValue in listObjectEachValue:
-                value = listValue[listObjectEachValue.index(listObjectByValue)]
-                dictEntropyOfValue[value] = self._calc_entropy(listObjectByValue)
-            attrName = self.listAttrName[posAttr]
-            dictEntropyAttr[attrName] = dictEntropyOfValue
-        print(dictEntropyAttr)
+        self._set_root()
+        queueSepNode = self._find_list_separate_node(self.root)
+        while queueSepNode:
+            sepNode = queueSepNode.pop(0)
+            queueSepNode.extend(self._find_list_separate_node(sepNode))
 
+    def _display_tree(self):
+        pass
+            
 
-        return 1
-    
+def _get_max_of_dict(d: dict):
+    max = 0
+    key = -1
+    for (k,v) in d.items():
+        if v > max:
+            max = v
+            key = k
+    return (key, max)
 
-
+def _get_min_of_dict(d: dict):
+    min = 0
+    key = -1
+    for (k,v) in d.items():
+        if v < min:
+            min = v
+            key = k
+    return (key, min)
     
 
 def _read_file(inputFolder: str, inputFileName: str) -> list:
@@ -85,9 +201,13 @@ def _read_file(inputFolder: str, inputFileName: str) -> list:
 
 
 def main():
-    listData = _read_file(folderInput, fileInput)
+    listData = _read_file(folderInput, fileInput[0])
     indexClassifyAttribute = -1
     id3 = DTreeID3(listData, indexClassifyAttribute)
     id3._run()
+    id3._display_tree()
+    print()
+
+    
 
 if __name__ == "__main__": main()

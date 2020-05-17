f7a81092c6ddac2ff2f971c50b2b2404e0174ff1
lamphucnghi313 <lamphucnghi@gmail.com>
2020-05-17 18:05:34 +0700

    Một vài tính năng ban đầu.

--- DTID3_App.py
+++ DTID3_App.py
@@ -11,55 +11,116 @@ class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
         uic.loadUi('DTID3_GUI.ui', self)
+        self.model = QtGui.QStandardItemModel(self)
 
         self.btnImport.clicked.connect(self.on_Import_clicked)
+        self.btnExport.clicked.connect(self.on_Export_clicked)
+        self.btnSave.clicked.connect(self.on_Save_clicked)
+        self.btnAdd.clicked.connect(self.on_Add_clicked)
         self.btnId3.clicked.connect(self.on_Id3_clicked)
         self.btnPredict.clicked.connect(self.on_Predict_clicked)
         self.lineEditPredict.setDisabled(True)
         self.lineEditPredict.textChanged.connect(self.on_textchanged)
-        
-        self.df = pd.DataFrame()
-        self.attributes = list()
-        self.tree = dict()
+        self.tableViewInput.setModel(self.model)
+        # self.tableViewInput.horizontalHeader().setStretchLastSection(True) #optional
 
+        self.fileName = ''
+        self.inputDir = './input/'
+        self.listInput = []
+        self.posClassify = -1
+        self.tree = DTID3_Class.DTreeID3(self.posClassify)
+
+        
     def on_textchanged(self):
         self.labelPredict.clear()
+
+    def on_Add_clicked(self):
+        self.model.appendRow([])
+
+    def on_Export_clicked(self):
+        (dataPath, _) = QtWidgets.QFileDialog.getSaveFileName(None, 'Save File', self.inputDir, '*.csv')
+        fileName = dataPath.split('/')[-1]
+        if dataPath:
+            self._export_file(fileName)
+
+    def _export_file(self, fileName: str):
+        with open(self.inputDir + fileName, 'w') as fileOutput:
+            writer = csv.writer(fileOutput, lineterminator='\n')
+            for rowNumber in range(self.model.rowCount()):
+                fields = []
+                for columnNumber in range(self.model.columnCount()):
+                    fields.append(self.model.data(self.model.index(rowNumber, columnNumber), QtCore.Qt.DisplayRole))
+                writer.writerow(fields)
+
+    def on_Save_clicked(self):
+        self._export_file(self.fileName)
+
+    def _import_file(self, fileName):
+        with open(self.inputDir + self.fileName, 'r', encoding = 'utf-8') as fileInput:
+            rCount = 0
+            for row in csv.reader(fileInput):    
+                # if row[0] not in (None, ""):
+                if row[0] != '':
+                    items = []
+                    cCount = 0
+                    for field in row:
+                        items.append(QtGui.QStandardItem(field))
+                        cCount += 1
+                    rCount += 1
+                    self.model.appendRow(items)
+        self.labelInputCount.setText('{}. {} dòng {} cột'.format(self.fileName, rCount, cCount))
+
+    def _get_list_from_QStandardItemModel(self) -> list:
+        listInput = []
+        for rowNumber in range(self.model.rowCount()):
+            row = []
+            for columnNumber in range(self.model.columnCount()):
+                cell = self.model.data(self.model.index(rowNumber, columnNumber), QtCore.Qt.DisplayRole)
+                row.append(cell)
+            listInput.append(row)
+        return listInput
+        
 
     def on_Import_clicked(self):
         self.labelInputCount.clear()
         self.labelInputName.setText('Dữ liệu')
         
-        inputDir = './input/'
-        (dataPath, _) = QtWidgets.QFileDialog.getOpenFileName(None, 'Open File', inputDir, '*.csv')
-        fileName = dataPath.split('/')[-1]
+        (dataPath, _) = QtWidgets.QFileDialog.getOpenFileName(None, 'Open File', self.inputDir, '*.csv')
+        self.fileName = dataPath.split('/')[-1]
         if dataPath:
-            dataFrame = ID3.import_data(inputDir, fileName)
+            self.model.clear()
+            self._import_file(self.fileName)
+            self.btnExport.setDisabled = False
 
-            self.attributes = list(dataFrame.columns.values)[: len(list(dataFrame.columns.values)) - 1]
+            self.listInput = self._get_list_from_QStandardItemModel()
+
+            self.plainTextDTree.clear()
             
-            model = DataFrameModel.DataFrameModel(dataFrame)
-            self.tableViewInput.setModel(model)
             
-            self.labelInputCount.setText('{} dòng. {} cột.'.format(model._dataframe.shape[0], model._dataframe.shape[1]) )
-            self.labelInputName.setText(dataPath.split('/')[-1])
-             
-            self.df = model._dataframe
-            
-            # Hint bang placeholdertext cho o lineEditText.
-            firstValueRow = list(dataFrame.iloc[0])[:len(list(dataFrame.iloc[0])) - 1]
-            firstRowStr = 'Ví dụ : '
-            for value in range(len(firstValueRow) - 1):
-                firstRowStr += str(firstValueRow[value]) + ','
-            firstRowStr += str(firstValueRow[-1])
-            firstRowStr = firstRowStr.rstrip()
-            self.lineEditPredict.setPlaceholderText(firstRowStr)
-            # set enabled lineeditPredict
-            self.lineEditPredict.setDisabled(False)
+
+            # # Hint bang placeholdertext cho o lineEditText.
+            # firstValueRow = list(dataFrame.iloc[0])[:len(list(dataFrame.iloc[0])) - 1]
+            # firstRowStr = 'Ví dụ : '
+            # for value in range(len(firstValueRow) - 1):
+            #     firstRowStr += str(firstValueRow[value]) + ','
+            # firstRowStr += str(firstValueRow[-1])
+            # firstRowStr = firstRowStr.rstrip()
+            # self.lineEditPredict.setPlaceholderText(firstRowStr)
+            # # set enabled lineeditPredict
+            # self.lineEditPredict.setDisabled(False)
+
 
     def on_Id3_clicked(self):
-        self.tree = ID3.buildTree(self.df)
-        pprint.pprint(self.tree)
-        # self.plainTextDTree.toPlainText(tree_str)
+        self.tree = DTID3_Class.DTreeID3(self.posClassify)
+        self.tree._set_inputData(self.listInput)
+        self.tree._run()
+        self.tree._get_all_branch()
+        strAllBranch = ''
+        for branch in self.tree.listBranchStr:
+            strAllBranch += branch + '\n' 
+        self.plainTextDTree.insertPlainText(strAllBranch)
+        # Hint ở ô dự đoán dữ liệu mới.
+
         
     def on_Predict_clicked(self):
         instanceDict = dict()

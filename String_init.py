d1b37dffd6f036fd8e31549f74ae6c7c90213c2c
lamphucnghi313 <lamphucnghi@gmail.com>
2020-05-24 22:50:58 +0700

    no message

---
+++ String_init.py
@@ -0,0 +1,6 @@
+folderInput = 'input'
+fileInput = ['weather', 'Bai2', 'BuyComputer', 'BHK', 'W', 'diabetes']
+folderOutFile = 'outfile'
+fileLog = 'log'
+fileType = ['.txt']
+splitType = [', ', ' ']
